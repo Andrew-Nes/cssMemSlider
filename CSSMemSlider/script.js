16e26f394fb8ce3409bc1d1e02a28a2b18bb9ab6
Andrei Niasmachny <Cejosoll@gmail.com>
2021-10-18 14:58:37 +0300

    refactor: correct mem image styles (18.10.2021 14.58)

--- CSSMemSlider/script.js
+++ CSSMemSlider/script.js
@@ -1,31 +1,31 @@
 const mem1 = {
     name: 'mem1',
     imgSrc: 'assets/images/1.jpg',
-    text : 'Find motivation, or one day it will find you.'
+    text : 'Find motivation, or one day it will find you'
 }
 
 const mem2 = {
     name: 'mem2',
     imgSrc: "assets/images/2.jpg",
-    text: 'Do not limit yourself.'
+    text: 'Do not limit yourself'
 }
 
 const mem3 = {
     name: 'mem3',
     imgSrc: "assets/images/3.jpg",
-    text: 'Beleive in yourself.'
+    text: 'Beleive in yourself'
 }
 
 const mem4 = {
     name: 'mem4',
     imgSrc: "assets/images/4.jpg",
-    text: 'Try to do a little bit more every day.'
+    text: 'Try to do a little bit more every day'
 }
 
 const mem5 = {
     name: 'mem5',
     imgSrc: "assets/images/5.jpg",
-    text: 'Stay strong and u can do it.'
+    text: 'Stay strong and u can do it'
 }
 
 const pages = document.querySelector('.pagination-container'); 
